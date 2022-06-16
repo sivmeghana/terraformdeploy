dfdbc542be2e65f81d6e8b9ce1164f43256dc1c4
sivmeghana <85859524+sivmeghana@users.noreply.github.com>
2022-06-16 10:12:25 +0530

    version changed

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 terraform {
 
-  required_version = ">=0.12"
+  required_version = "latest"
 
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~>2.0"
+      version = "latest"
     }
   }
 }
